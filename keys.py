ce8de669d5ae196bc90b956d60a19070fc1df21d
acodered <alexkondratru@gmail.com>
2024-05-29 23:14:00 +0200

    Add docstrings to keys classes

--- keys.py
+++ keys.py
@@ -2,15 +2,20 @@
 
 
 class AbstractKey:
+    """Interface class for RSA algorithm keys."""
+
     def __init__(self, e: int, n: int) -> None:
         self.e = e
         self.n = n
 
-    
+
 class PublicKey(AbstractKey):
-    pass
+    """Public key for the RSA algorithm."""
 
 
 class PrivateKey(AbstractKey):
+    """Private key for the RSA algorithm."""
+
     def __init__(self, e: int, n: int, d: int) -> None:
         super().__init__(e, n)
+        self.d = d
