69651f6b1571f568bf453527809292b34f99d4d6
acodered <alexkondratru@gmail.com>
2024-05-29 23:15:19 +0200

    Add generate_keypair() function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """Test functions for the RSA algorithm."""
 
 import random
+from typing import Tuple
 import sympy
+from keys import PublicKey, PrivateKey
+
+
+DEFAULT_EXPONENT = 65537
 
 
 def generate_prime_candidate(length: int) -> int:
@@ -53,9 +58,28 @@ def generate_prime_number(length: int) -> int:
             return p
 
 
+def generate_keypair(
+    length: int, exponent: int = DEFAULT_EXPONENT
+) -> Tuple[PublicKey, PrivateKey]:
+    """
+    Generate a public-private key pair.
+    """
+    e = exponent
+    p = generate_prime_number(length // 2)
+    q = generate_prime_number(length // 2)
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+    # find such d that (d * e) % phi_n = 1
+    d = sympy.mod_inverse(e, phi_n)
+    public_key = PublicKey(e, n)
+    private_key = PrivateKey(e, n, d)
+
+    return public_key, private_key
+
+
 def main():
     """Run the test functions."""
-    print(generate_prime_number(13))
+    public_key, private_key = generate_keypair(2048)
 
 
 if __name__ == "__main__":
