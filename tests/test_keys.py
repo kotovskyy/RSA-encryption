bea90f01097202caae20f4a06c1e6eebb21a333b
acodered <alexkondratru@gmail.com>
2024-05-30 13:06:43 +0200

    Add test_generate_p_q_accurate()

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -24,3 +24,13 @@ def test_generate_p_q_inaccurate(length: int):
     assert p != q
     assert p.bit_length() == length//2 
     assert q.bit_length() == length//2
+
+
+@pytest.mark.parametrize("length", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
+def test_generate_p_q_accurate(length: int):
+    p, q = rsa.keys.generate_p_q(length, accurate=True)
+    assert sympy.isprime(p)
+    assert sympy.isprime(q)
+    assert p != q
+    assert p.bit_length() == length//2 
+    assert q.bit_length() == length//2
