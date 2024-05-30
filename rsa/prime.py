3078a0f17ebdbf71aea4fcf61891bd1c1104ea83
acodered <alexkondratru@gmail.com>
2024-05-30 12:01:51 +0200

    Move primes related funcs to rsa/prime.py

---
+++ rsa/prime.py
@@ -0,0 +1,53 @@
+"Functinons dealing with prime numbers."
+
+import random
+import sympy
+
+
+def generate_prime_candidate(length: int) -> int:
+    """
+    Generate a prime candidate number.
+    The number is odd and has exactly `length` bits.
+
+    Args:
+        - `length: int`: The number of bits the prime should have.
+
+    Returns:
+        - `int`: The prime candidate number.
+
+    Example:
+    >>> length = 8
+    >>> candidate = generate_prime_candidate(length)
+    >>> print(bin(candidate))
+    '0b11101011' # Example output, actual output will vary
+    """
+    p = random.getrandbits(length)
+    # Set the most significant bit to 1
+    msb_and_lsb = 1 << length - 1
+    # Set the least significant bit to 1
+    msb_and_lsb |= 1
+    p |= msb_and_lsb
+
+    return p
+
+
+def generate_prime_number(length: int) -> int:
+    """
+    Generate a prime number.
+
+    Args:
+        - `length: int`: The number of bits the prime should have.
+
+    Returns:
+        - `int`: The prime number.
+
+    Example:
+    >>> length = 8
+    >>> prime = generate_prime_number(length)
+    >>> print(bin(prime))
+    '0b11000101' # 197, example output, actual output will vary
+    """
+    while True:
+        p = generate_prime_candidate(length)
+        if sympy.isprime(p):
+            return p
