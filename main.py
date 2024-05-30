c8c265dae6c07283d54f9b29f44bc399b3496a6b
acodered <alexkondratru@gmail.com>
2024-05-30 11:01:14 +0200

    Add text encryption/decryption

--- main.py
+++ main.py
@@ -147,12 +147,36 @@ def encrypt(public_key: PublicKey, message: int) -> int:
     return pow(message, public_key.e, public_key.n)
 
 
+def text_to_int(text: str) -> int:
+    """Convert a string to an integer."""
+    return int.from_bytes(text.encode("utf-8"), byteorder="big")
+
+
+def int_to_text(number: int) -> str:
+    """Convert an integer to a string."""
+    return number.to_bytes((number.bit_length() + 7) // 8, byteorder="big").decode(
+        "utf-8"
+    )
+
+
+def encrypt_text(public_key: PublicKey, message: str) -> int:
+    """Encrypt a text message using a public key."""
+    message = text_to_int(message)
+    return encrypt(public_key, message)
+
+
+def decrypt_text(private_key: PrivateKey, message: int) -> str:
+    """Decrypt a text message using a private key."""
+    message = decrypt(private_key, message)
+    return int_to_text(message)
+
+
 def main():
     """Run the test functions."""
     public_key, private_key = generate_keypair(2048)
-    message = 1234
-    encrypted_message = encrypt(public_key, message)
-    decrypted_message = decrypt(private_key, encrypted_message)
+    message = "Hello there!"
+    encrypted_message = encrypt_text(public_key, message)
+    decrypted_message = decrypt_text(private_key, encrypted_message)
     print(f"Original message: {message}")
     print(f"Encrypted message: {encrypted_message}")
     print(f"Decrypted message: {decrypted_message}")
