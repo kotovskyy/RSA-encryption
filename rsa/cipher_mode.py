45c135873f41e722a311e7a05a67209703fd6fd1
Vizels <mirchuk.georgiy@gmail.com>
2024-06-04 00:45:51 +0200

    additional_pad moved inside class (self.additional_pad)

--- rsa/cipher_mode.py
+++ rsa/cipher_mode.py
@@ -8,11 +8,15 @@ from binascii import hexlify
 class BaseMode:
     """Interface class for block cipher modes of operation."""
 
-    def __init__(self, publick_key: PublicKey, private_key: PrivateKey):
+    def __init__(self, publick_key: PublicKey, private_key: PrivateKey, additional_pad: bool = True):
         self.public_key = publick_key
         self.private_key = private_key
         self.key_size = (publick_key.n.bit_length() + 7) // 8
-        self.block_size = self.key_size - 11  # min 11 bytes for padding
+        self.additional_pad = additional_pad
+        if additional_pad:
+            self.block_size = self.key_size - 11  # min 11 bytes for padding
+        else:
+            self.block_size = self.key_size - 1
 
     def encrypt(self, data: bytes) -> bytes:
         """Encrypt the data."""
@@ -52,7 +56,7 @@ class BaseMode:
 
         return padding + data_block
 
-    def encrypt_block(self, data_block: bytes, additional_pad: bool = True) -> bytes:
+    def encrypt_block(self, data_block: bytes) -> bytes:
         """
         Encrypt a single block of data. The block size is determined by the key size.
 
@@ -62,7 +66,7 @@ class BaseMode:
         Returns:
             - `bytes`: The encrypted data block.
         """
-        if additional_pad:
+        if self.additional_pad:
             data_block = self.add_padding(data_block)
         return encrypt(self.public_key, int.from_bytes(data_block, byteorder="big"))
 
@@ -83,7 +87,7 @@ class BaseMode:
 
         return data_block[padding_end + 1 :]
 
-    def decrypt_block(self, data_block: bytes, additional_pad: bool = True) -> bytes:
+    def decrypt_block(self, data_block: bytes) -> bytes:
         """
         Decrypt a single block of data. The block size is determined by the key size.
 
@@ -96,7 +100,7 @@ class BaseMode:
         decrypted_data = decrypt(
             self.private_key, int.from_bytes(data_block, byteorder="big")
         )
-        if additional_pad:
+        if self.additional_pad:
             decrypted_data = self.remove_padding(decrypted_data)
         return decrypted_data
 
@@ -104,7 +108,7 @@ class BaseMode:
 class ECB(BaseMode):
     """Electronic Codebook (ECB) mode for RSA encryption."""
 
-    def encrypt(self, data: bytes, additional_pad: bool = True) -> bytes:
+    def encrypt(self, data: bytes) -> bytes:
         """
         Encrypt the data using the ECB mode.
 
@@ -114,19 +118,19 @@ class ECB(BaseMode):
         Returns:
             - `bytes`: The encrypted data.
         """
-        self.block_size = self.key_size - 11 if additional_pad else self.key_size - 1
+        self.block_size = self.key_size - 11 if self.additional_pad else self.key_size - 1
         encrypted_data = b""
         for i in range(0, len(data), self.block_size):
             data_block = data[i : i + self.block_size]
-            if not additional_pad:
+            if not self.additional_pad:
                 data_block = b"\x01" + data_block
                 
-            encrypted_block = self.encrypt_block(data_block, additional_pad)
+            encrypted_block = self.encrypt_block(data_block)
             encrypted_data += encrypted_block
 
         return encrypted_data
 
-    def decrypt(self, data: bytes, additional_pad: bool = True) -> bytes:
+    def decrypt(self, data: bytes) -> bytes:
         """
         Decrypt the data using the ECB mode.
 
@@ -139,8 +143,8 @@ class ECB(BaseMode):
         decrypted_data = b""
         for i in range(0, len(data), self.key_size):
             data_block = data[i : i + self.key_size]
-            decrypted_block = self.decrypt_block(data_block, additional_pad)
-            if not additional_pad:
+            decrypted_block = self.decrypt_block(data_block)
+            if not self.additional_pad:
                 if i + self.key_size >= len(data):
                     decrypted_block = decrypted_block.lstrip(b"\x00")
                 decrypted_block = decrypted_block[1:]
@@ -153,7 +157,7 @@ class ECB(BaseMode):
 class CBC(BaseMode):
     """Cipher Block Chaining (CBC) mode for RSA encryption."""
 
-    def encrypt(self, data: bytes, additional_pad: bool = True) -> bytes:
+    def encrypt(self, data: bytes) -> bytes:
         """
         Encrypt the data using the CBC mode.
 
@@ -163,26 +167,24 @@ class CBC(BaseMode):
         Returns:
             - `bytes`: The encrypted data.
         """
-        self.block_size = self.key_size - 11 if additional_pad else self.key_size - 1
+        self.block_size = self.key_size - 11 if self.additional_pad else self.key_size - 1
         initial_vector = random.randbytes(self.key_size)
         encrypted_data = b"" + initial_vector
         for i in range(0, len(data), self.block_size):
             data_block = data[i : i + self.block_size]
-            if not additional_pad:
+            if not self.additional_pad:
                 data_block = b"\x01" + data_block
                 if i + self.block_size >= len(data):
                     data_block = b'\x00' * (self.key_size - len(data_block)) + data_block
                     
             encrypted_block = self.encrypt_block(
-                bytes(b1 ^ b2 for b1, b2 in zip(data_block, initial_vector)),
-                additional_pad,
-            )
+                bytes(b1 ^ b2 for b1, b2 in zip(data_block, initial_vector)))
             encrypted_data += encrypted_block
             initial_vector = encrypted_block
 
         return encrypted_data
 
-    def decrypt(self, data: bytes, additional_pad: bool = True) -> bytes:
+    def decrypt(self, data: bytes) -> bytes:
         """
         Decrypt the data using the CBC mode.
 
@@ -196,11 +198,11 @@ class CBC(BaseMode):
         decrypted_data = b""
         for i in range(self.key_size, len(data), self.key_size):
             data_block = data[i : i + self.key_size]
-            decrypted_block = self.decrypt_block(data_block, additional_pad)
+            decrypted_block = self.decrypt_block(data_block)
             decrypted_block = bytes(
                 b1 ^ b2 for b1, b2 in zip(decrypted_block, initial_vector)
             )
-            if not additional_pad:
+            if not self.additional_pad:
                 if i + self.key_size >= len(data):
                     decrypted_block = decrypted_block.lstrip(b"\x00")
                 decrypted_block = decrypted_block[1:]
@@ -214,7 +216,7 @@ class CBC(BaseMode):
 
 class CTR(BaseMode):
     """Counter (CTR) mode for RSA encryption."""
-    def encrypt(self, data: bytes, additional_pad: bool = False) -> bytes:
+    def encrypt(self, data: bytes) -> bytes:
         """
         Encrypt the data using the CBC mode.
 
@@ -224,12 +226,12 @@ class CTR(BaseMode):
         Returns:
             - `bytes`: The encrypted data.
         """
-        self.block_size = self.key_size - 11 if additional_pad else self.key_size
+        self.block_size = self.key_size - 11 if self.additional_pad else self.key_size
         nonce = random.randbytes(self.block_size)
         encrypted_data = b"" + nonce
         for i in range(0, len(data), self.block_size):
             data_block = data[i : i + self.block_size]
-            encrypted_counter = self.encrypt_block(nonce, additional_pad)
+            encrypted_counter = self.encrypt_block(nonce)
             if len(data_block) != self.block_size:
                 data_block = b"\x00" * (self.block_size - len(data_block)) + data_block
             encrypted_block = bytes(b1 ^ b2 for b1, b2 in zip(data_block, encrypted_counter))
@@ -239,7 +241,7 @@ class CTR(BaseMode):
 
         return encrypted_data
 
-    def decrypt(self, data: bytes, additional_pad: bool = False) -> bytes:
+    def decrypt(self, data: bytes) -> bytes:
         """
         Decrypt the data using the CBC mode.
 
@@ -249,15 +251,15 @@ class CTR(BaseMode):
         Returns:
             - `bytes`: The decrypted data.
         """
-        self.block_size = self.key_size - 11 if additional_pad else self.key_size
+        self.block_size = self.key_size - 11 if self.additional_pad else self.key_size
         nonce = data[0 : self.block_size]
         decrypted_data = b""
         for i in range(self.block_size, len(data), self.key_size):
             data_block = data[i : i + self.key_size]
-            encrypted_counter = self.encrypt_block(nonce, additional_pad)
+            encrypted_counter = self.encrypt_block(nonce)
             decrypted_block = bytes(b1 ^ b2 for b1, b2 in zip(data_block, encrypted_counter))
             print(f"Decrypted block len: {len(decrypted_block)}")
-            if i + self.key_size >= len(data) and not additional_pad:
+            if i + self.key_size >= len(data) and not self.additional_pad:
                 decrypted_block = decrypted_block.lstrip(b"\x00")
             decrypted_data += decrypted_block
             nonce = int.to_bytes(int.from_bytes(nonce, byteorder="big") + 1, self.block_size, byteorder="big")
